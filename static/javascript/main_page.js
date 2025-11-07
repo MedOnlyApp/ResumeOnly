2e8a613bee50adedab46b317926923be763a0637
Mohamed Rouane <131095232+MedOnlyApp@users.noreply.github.com>
2025-11-07 17:02:19 +0100

    Update main_page.js

--- static/javascript/main_page.js
+++ static/javascript/main_page.js
@@ -2,7 +2,6 @@
 console.log(location.href)
 
 window.addEventListener('popstate', function (event) {
-    console.log('popstate:', event.state);
     history.pushState(null, null, '/login');
 
     alert("Back button is disabled on this page.");
@@ -12,5 +11,6 @@ window.history.forward();
 function noBack() {
     window.history.forward();
 }
+
 
 
